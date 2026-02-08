2f583a7f3d8d9b9efa3c3230de287d66391d1f3b
Eduardo Rezende <eduardorbr7@gmail.com>
2026-02-08 01:37:45 -0300

    feat(object): add is plain object and remove nullish values functions
    Adds the function isPlainObject() to check if an object has a prototype
    that is either Object or null.
    
    Adds the function removeNullishValues() which removes null and undefined
    values from arrays and plain objects.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { jsonParse, jsonStringify } from './lib/json';
+export { isPlainObject, removeNullishValues } from './lib/object';
---
+++ src/lib/object.spec.ts
@@ -0,0 +1,52 @@
+import { isPlainObject, removeNullishValues } from './object';
+
+const invalidPlainObjects: any[] = [
+  undefined,
+  null,
+  true,
+  1,
+  1.2,
+  1n,
+  'a',
+  Symbol('a'),
+  Buffer,
+  Buffer.alloc(1),
+  () => 1,
+  [],
+];
+
+describe('isPlainObject()', () => {
+  it.each(invalidPlainObjects)('should return false when the data is not a plain javascript object.', (data) => {
+    expect(isPlainObject(data)).toBeFalse();
+  });
+
+  it.each([{}, Object.create(null)])('should return true when the data is a plain javascript object.', (data) => {
+    expect(isPlainObject(data)).toBeTrue();
+  });
+});
+
+describe('removeNullishValues()', () => {
+  it('should remove all undefined values from an object.', () => {
+    const data: object[] = [
+      {
+        name: 'John Doe',
+        occupation: null,
+        vehicle: undefined,
+        age: 23,
+        address: { streetAddress: '123 1st Avenue', referencePoint: null, owner: undefined, trees: ['Oak', null] },
+        hobbies: ['Jogging', undefined, { name: 'Gambling', skills: ['Poker', 'Black Jack'] }],
+      },
+    ];
+
+    expect(() => removeNullishValues(data)).not.toThrow();
+
+    expect(data).toStrictEqual<object[]>([
+      {
+        name: 'John Doe',
+        age: 23,
+        address: { streetAddress: '123 1st Avenue', trees: ['Oak'] },
+        hobbies: ['Jogging', { name: 'Gambling', skills: ['Poker', 'Black Jack'] }],
+      },
+    ]);
+  });
+});
